f14e42beedd85bec8c8f5c3343427edba754aab0
anago <outpe1119@gmail.com>
2023-08-27 13:13:24 +0900

    APITableを実装

--- app/src/main/java/com/anago/apkeditor/MainActivity.kt
+++ app/src/main/java/com/anago/apkeditor/MainActivity.kt
@@ -2,21 +2,36 @@ package com.anago.apkeditor
 
 import android.content.Intent
 import android.os.Bundle
+import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
 import com.anago.apkeditor.applist.AppListActivity
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+    
         val btnInstalledApp: MaterialButton = findViewById(R.id.btn_installedApp)
         val btnAPITable: MaterialButton = findViewById(R.id.btn_api_table)
-        
+    
         btnInstalledApp.setOnClickListener {
             val intent = Intent(this, AppListActivity::class.java)
             startActivity(intent)
         }
+    
+        btnAPITable.setOnClickListener {
+            showAPITableDialog()
+        }
+    }
+    
+    private fun showAPITableDialog() {
+        val webView = WebView(this).apply {
+            loadUrl("file:///android_asset/apitable.html")
+        }
+        MaterialAlertDialogBuilder(this).apply {
+            setView(webView)
+        }.show()
     }
 }
